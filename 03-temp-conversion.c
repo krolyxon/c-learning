2329ae89e9165b769d2671711d463047116f7583
krolyxon <krolyxon@tutanota.com>
2023-03-04 13:10:25 +0530

    temp conversion

--- 03-temp-conversion.c
+++ 03-temp-conversion.c
@@ -1,17 +1,45 @@
 #include <stdio.h>
 
-// Converting Fahrenheit to celcius
-// 0 fahrenheit = -17.77 degree celcius
-// therefore, celcius = ( fahrenheit - 32 ) * 5/9
+int choice, celcius, fahrenheit;
 
-void ftoc(float temp) {
-    double tmp_in_celcius = (( temp - 32) * 5/9);
-    printf("Conversion of %2.2f °F in Celcius is %2.2f °C", temp, tmp_in_celcius);
+void fah_to_cel() {
+    printf("\nPlease enter the temperature in Fahrenheit: ");
+    scanf("%d", &fahrenheit);
+    printf("\nThe temperature in Celcius is: %f", (fahrenheit - 32)/1.8);
 }
 
-int main() {
-    float temp;
-    printf("Enter the temperature in fahrenheit: ");
-    scanf("%f", &temp);
-    ftoc(temp);
+void cel_to_fah() {
+    printf("\nPlease enter the temperature in Celcius: ");
+    scanf("%d", &celcius);
+    printf("\nThe temperature in Fahrenheit is: %f", (celcius * 1.8 ) + 32);
+}
+
+void fah_to_kel() {
+    printf("\nPlease enter the temperature in Fahrenheit: ");
+    scanf("%d", &fahrenheit);
+    printf("\nThe temperature in kelvin is: %f", (fahrenheit + 459.67 ) * 5/9 );
+}
+
+void cel_to_kel() {
+    printf("\nPlease enter the temperature in celcius: ");
+    scanf("%d", &celcius);
+    printf("\nThe temperature in kelvin is: %f", (celcius + 273.15 ));
+}
+
+int main()
+{
+    printf("\n[1] Convert Fahrenheit to Celcius");
+    printf("\n[2] Convert Celcius to Fahrenheit");
+    printf("\n[3] Convert Fahrenheit to Kelvin");
+    printf("\n[4] Convert Celcius to Kelvin");
+
+    printf("\nPlease choose a method from above: ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1: fah_to_cel(); break;
+        case 2: cel_to_fah(); break;
+        case 3: fah_to_kel(); break;
+        case 4: cel_to_kel(); break;
+    }
 }
