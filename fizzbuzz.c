f5e045ef299b60498e6233f6854cdfa4c9ac7234
krolyxon <krolyxon@tutanota.com>
2023-03-07 17:53:00 +0530

    user input instead of constant

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int i, n = 30;
-    for ( i=0; i<n; i++){
+    int i, n;
+    printf("Please enter a number: ");
+    scanf("%d", &n);
+    for ( i=0; i<=n; i++){
         if (i % 3 == 0 && i % 5 == 0) {
             printf("\nfizzbuzz");
         } else if (i % 3 == 0) {
