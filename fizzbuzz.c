7c94c1ed3fce04fa4c77f44dcd3aabce755c52ab
krolyxon <krolyxon@tutanota.com>
2023-03-07 17:22:39 +0530

    iterate over number of numbers instead of array

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int i, number[15] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-    for ( i=0; i<15; i++){
-        if (number[i] % 3 == 0 && number[i] % 5 == 0) {
+    int i, n = 30;
+    for ( i=0; i<n; i++){
+        if (i % 3 == 0 && i % 5 == 0) {
             printf("\nfizzbuzz");
-        } else if (number[i] % 3 == 0) {
+        } else if (i % 3 == 0) {
             printf("\nfizz");
-        } else if (number[i] % 5 == 0) {
+        } else if (i % 5 == 0) {
             printf("\nbuzz");
         } else {
-            printf("\n%d", number[i]);
+            printf("\n%d", i);
         }
     }
 }
