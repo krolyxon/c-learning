df7e7ae533156e85d85a33471f2c2ff79518a06f
krolyxon <krolyxon@tutanota.com>
2023-03-11 21:19:50 +0530

    triangle spaces

--- triangle.c
+++ triangle.c
@@ -5,10 +5,12 @@ int main() {
     printf("Please Enter a number: ");
     scanf("%d", &n);
     for (i=1; i<=n; i++) {
+        for (j=1; j<=n-i;j++) {
+            printf(" ");
+        }
         for (j=1; j<=i;j++) {
-            printf("*");
+            printf("* ");
         }
         printf("\n");
-
     }
 }
